f82e45147ca1d2e13fc8c949c7cc0c2686e1232c
BuTaMuH4uK <butamuh4uk98@mail.ru>
2017-10-23 00:48:06 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,30 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if(arguments.length) {
+			this.maxSize = maxSize;
+		} else {
+			this.maxSize = 30;
+		}
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if(this.heap.size() == this.maxSize) {throw Error; return;}
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if(this.heap.isEmpty()) {throw Error; return;}
+		return this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
